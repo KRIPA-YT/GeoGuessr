c01b16cf153ec9c30faf473c1011083362033f4e
Amethyst <hbqacc@gmail.com>
2024-04-24 22:37:34 +0200

    Basic Parallel Downloading

--- src/main.py
+++ src/main.py
@@ -1,17 +1,21 @@
 from mapillary_api import *
-from PIL import Image
 from sys import argv
 
 
 def main():
     api = MapillaryAPI(argv[1])
-    # Somewhere in Antwerp, Belgium
-    responses = api.search(51.1543669, 4.4436676, 0.00025, 0.00025)
+    # Somewhere in Heidelberg, Germany
+    responses = api.search(49.395665034392536, 8.599463334284934,
+                           0.00025, 0.00025, parallels=20)
     print(f'Total Images: {len(responses)}')
-    for response in responses:
+    for i, response in enumerate(responses):
         picture = response.get_picture()
-        print(f'Picture Dimensions: {picture.size}')
-        print(response.get_coordinates())
+        dimension = picture.size
+        coordinates = response.get_coordinates()
+        print('#{:2.0f} {}x{}   {:<18} {:<18}'
+              .format(i,
+                      dimension[0], dimension[1],
+                      str(coordinates[0]) + "°N", str(coordinates[1]) + "°E"))
 
 
 if __name__ == "__main__":
--- src/mapillary_api.py
+++ src/mapillary_api.py
@@ -1,18 +1,36 @@
+import itertools
+
 import requests
 from PIL import Image
 import io
+from geopy.geocoders import Nominatim
+import multiprocessing
+
+
+def download(url, headers):
+    point_url = MapillaryAPI.METADATA_ENDPOINT + (f"/{url['id']}"
+                                                  f"?fields=id,thumb_original_url,captured_at,geometry")
+    point_response = requests.get(point_url, headers=headers)
+    point_json = point_response.json()
+    image_url = point_json['thumb_original_url']
+    image_content = requests.get(image_url).content
+    image = Image.open(io.BytesIO(image_content))
+    return MapillaryResponse(point_json['id'], image_url, image,
+                             point_json['captured_at'], point_json['geometry'])
 
 
 class MapillaryResponse:
-    def __init__(self, id, url, timestamp, geometry):
+    __GEOLOCATOR = Nominatim(user_agent="geoguessr_ai")
+
+    def __init__(self, id, url, image, timestamp, geometry):
         self.id = id
         self.url = url
+        self.image = image
         self.timestamp = timestamp
         self.geometry = geometry
 
     def get_picture(self) -> Image:
-        img_data = requests.get(self.url).content
-        return Image.open(io.BytesIO(img_data))
+        return self.image
 
     def get_coordinates(self) -> tuple[int, int]:
         if self.geometry['type'] != 'Point':
@@ -21,9 +39,14 @@ class MapillaryResponse:
         coordinates.reverse()
         return tuple[int, int](coordinates)
 
+    def get_address(self) -> str:
+        coordinates = list(self.get_coordinates())
+        coordinates.reverse()
+        return self.__GEOLOCATOR.reverse(coordinates)
+
 
 class MapillaryAPI:
-    __METADATA_ENDPOINT = "https://graph.mapillary.com"
+    METADATA_ENDPOINT = "https://graph.mapillary.com"
 
     def __init__(self, token):
         self.__token = token
@@ -32,9 +55,10 @@ class MapillaryAPI:
     def search(self,
                latitude_degrees, longitude_degrees,
                latitude_distance_degrees, longitude_distance_degrees,
-               *, amount=-1)\
+               *, amount=-1, parallels=None) \
             -> list[MapillaryResponse]:
-        url_imagesearch = (self.__METADATA_ENDPOINT + '/images?fields=id&bbox={},{},{},{}'
+
+        url_imagesearch = (self.METADATA_ENDPOINT + '/images?fields=id&bbox={},{},{},{}'
                            .format(longitude_degrees - longitude_distance_degrees,
                                    latitude_degrees - latitude_distance_degrees,
                                    longitude_degrees + longitude_distance_degrees,
@@ -42,15 +66,12 @@ class MapillaryAPI:
         imagesearch_response = requests.get(url_imagesearch, headers=self.__headers)
         imagesearch_json = imagesearch_response.json()
 
-        images = []
-
         amount = len(imagesearch_json['data']) if amount == -1 else amount
 
-        for image in imagesearch_json['data'][:amount]:
-            # DOCS: https://www.mapillary.com/developer/api-documentation/#image
-            image_url = self.__METADATA_ENDPOINT + f"/{image['id']}?fields=id,thumb_original_url,captured_at,geometry"
-            image_response = requests.get(image_url, headers=self.__headers)
-            image_json = image_response.json()
-            images.append(MapillaryResponse(image_json['id'], image_json['thumb_original_url'],
-                                            image_json['captured_at'], image_json['geometry']))
+        # Create download queues
+        urls = imagesearch_json['data'][:amount]
+
+        with multiprocessing.Pool() as pool:
+            images = pool.starmap(download, zip(urls, itertools.repeat(self.__headers)), chunksize=parallels)
+
         return images
